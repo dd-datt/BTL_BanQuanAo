6a640558262fa262891ef7cdae359836e3a9280b
Duyyn <duya9654@gmail.com>
2024-11-13 07:04:38 +0700

    UPDATE trang thanks

--- views/thanks.php
+++ views/thanks.php
@@ -1,14 +1,38 @@
-<div class="d-flex justify-content-center align-items-center" style="height: 400px; margin-bottom: 300px;">
-    <div>
-        <div class="mb-4 text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="text-success" width="75" height="75" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-            </svg>
-        </div>
-        <div class="text-center">
-            <h3 class="mb-2">Cảm ơn quý khách hàng đã mua hàng !</h3>
-            <h5 class="mb-3">Rất mong quý khách hàng sẽ luôn luôn tin tưởng và ủng hộ Fashion Store</h5>
-            <a href="don-hang" class="btn btn-primary">Xem đơn hàng</a>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thông báo</title>
+    <!-- CDN Bootstrap 4.6.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <style>
+        .success-message {
+            height: 400px;
+            margin-bottom: 300px;
+        }
+        .btn-primary {
+            color: #fff;
+            background-color: #00704A ;
+            border-color: #00704A;
+        }
+    </style>
+</head>
+<body>
+    <div class="d-flex justify-content-center align-items-center success-message">
+        <div>
+            <div class="mb-4 text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="text-success" width="75" height="75" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                </svg>
+            </div>
+            <div class="text-center">
+                <h3 class="mb-2">Cảm ơn quý khách hàng đã mua hàng!</h3>
+                <h5 class="mb-3">Rất mong quý khách hàng sẽ luôn luôn tin tưởng và ủng hộ Fashion Store</h5>
+                <a href="don-hang" class="btn btn-primary">Xem đơn hàng</a>
+            </div>
         </div>
     </div>
-</div>
+
+</body>
+</html>
