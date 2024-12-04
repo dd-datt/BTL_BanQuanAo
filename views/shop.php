16e20ce18f1cb4671c3860a8b97f7be2eb737c81
Nguyen Van Dat <vdat319@gmail.com>
2024-12-04 20:22:12 +0700

    thêm phần tìm kiếm bên phải

--- views/shop.php
+++ views/shop.php
@@ -34,6 +34,25 @@ $list_catgories = $CategoryModel->select_all_categories();
                     </div>
                 </div>
             </div>
+
+
+            <!-- Bên phải dùng để tìm kiếm-->
+            <div class="col-lg-9 col-md-9">
+                <div class="container-fluid mt-5">
+                    <div class="row rounded justify-content-center mx-0 pt-5">
+                        <div class="col-md-6 text-center">
+                            <!-- Form tìm -->
+                            <form action="tim-kiem" method="get">
+                                <div class="form-outline">
+                                    <input type="search" name="query" class="form-control" placeholder="Tìm kiếm" />
+                                </div>
+                            </form>
+                            <!-- Kthuc form -->
+                            <a class="btn btn-primary rounded-pill py-3 px-5 mt-5" href="index.php?url=cua-hang">Trở lại cửa hàng</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
